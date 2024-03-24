7a7505dbd4d4da7a7c5779ab9c8fd96c75cb6c75
AntonSakharovAE <anton@alefeducation.com>
2024-03-24 15:18:16 +0400

    add 1 empty task

---
+++ cmd/arrays/subarray_sum_equals_k/subarray_sum_equals_k.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func subarraySum(arr []int, k int) int {
+	return -1
+}
+
+func main() {
+
+	fmt.Println(subarraySum([]int{1, 1, 1}, 2))
+
+	fmt.Println(subarraySum([]int{1, 2, 3}, 3))
+
+}
