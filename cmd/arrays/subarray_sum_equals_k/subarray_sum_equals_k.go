08120be44e652aec21229b24dd5fa1f60d86231e
AntonSakharovAE <anton@alefeducation.com>
2024-03-26 09:48:24 +0400

    impl. subarray sum

--- cmd/arrays/subarray_sum_equals_k/subarray_sum_equals_k.go
+++ cmd/arrays/subarray_sum_equals_k/subarray_sum_equals_k.go
@@ -2,14 +2,31 @@ package main
 
 import "fmt"
 
+// O(n) time | O(n) space
 func subarraySum(arr []int, k int) int {
-	return -1
+
+	mp := make(map[int]int, 0)
+	mp[0] = 1
+	count := 0
+	sum := 0
+
+	for _, num := range arr {
+		sum += num
+
+		n, ok := mp[sum-k]
+		if ok {
+			count += n
+		}
+
+		mp[sum] = mp[sum] + 1
+	}
+	return count
 }
 
 func main() {
 
-	fmt.Println(subarraySum([]int{1, 1, 1}, 2))
+	fmt.Println(subarraySum([]int{1, 1, 1}, 2)) // 2
 
-	fmt.Println(subarraySum([]int{1, 2, 3}, 3))
+	fmt.Println(subarraySum([]int{1, 2, 3}, 3)) // 2
 
 }
