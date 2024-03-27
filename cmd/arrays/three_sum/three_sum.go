1f687547e67692a7712abe51396acccb8e3e0ee0
AntonSakharovAE <anton@alefeducation.com>
2024-03-27 10:30:10 +0400

    impl. three sum.

--- cmd/arrays/three_sum/three_sum.go
+++ cmd/arrays/three_sum/three_sum.go
@@ -1,13 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
+// O(n ^ 3) time | O(n) space
 func threeNumberSum(arr []int, target int) [][3]int {
-	return [][3]int{}
+	res := make([][3]int, 0)
+
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			for k := j + 1; k < len(arr); k++ {
+				if arr[i]+arr[j]+arr[k] == target {
+					res = append(res, [3]int{arr[i], arr[j], arr[k]})
+				}
+			}
+		}
+	}
+	return res
 }
 
+// O(n ^ 2) time | O(n) space
 func threeNumberSum1(arr []int, target int) [][3]int {
-	return [][3]int{}
+	sort.Ints(arr)
+
+	res := make([][3]int, 0)
+
+	for i := 0; i < len(arr); i++ {
+		if i > 0 && arr[i-1] == arr[i] {
+			continue
+		}
+
+		l := i + 1
+		r := len(arr) - 1
+
+		for l < r {
+			sum := arr[l] + arr[r] + arr[i]
+
+			if sum == target {
+				res = append(res, [3]int{arr[i], arr[l], arr[r]})
+				l++
+				r--
+				for l < r && arr[l-1] == arr[l] {
+					l++
+				}
+				for l < r && arr[r] == arr[r+1] {
+					r--
+				}
+			} else if sum < target {
+				l++
+			} else {
+				r--
+			}
+		}
+	}
+
+	return res
 }
 
 func main() {
