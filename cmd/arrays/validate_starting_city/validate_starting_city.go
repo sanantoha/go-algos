f06ccdd0f5ae406016e9ed95fb089fcf2e404607
AntonSakharovAE <anton@alefeducation.com>
2024-03-28 20:04:08 +0400

    impl. validate starting city.

--- cmd/arrays/validate_starting_city/validate_starting_city.go
+++ cmd/arrays/validate_starting_city/validate_starting_city.go
@@ -1,13 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+// O(n ^ 2) time | O(1) space
 func validateStartingCity(distances []int, fuel []int, mpg int) int {
+	if distances == nil || len(distances) == 0 || fuel == nil || len(fuel) == 0 {
+		return -1
+	}
+
+	for city := 0; city < len(distances); city++ {
+		remainingDistance := 0
+		for idx := city; idx < city+len(distances); idx++ {
+			ridx := idx % len(distances)
+			remainingDistance += fuel[ridx]*mpg - distances[ridx]
+
+			if remainingDistance < 0 {
+				break
+			}
+		}
+		if remainingDistance >= 0 {
+			return city
+		}
+	}
 	return -1
 }
 
+// O(n) time | O(1) space
 func validateStartingCity1(distance []int, fuel []int, mpg int) int {
-	return -1
+
+	remainingDistance := 0
+	minDistance := math.MaxInt
+	resCity := 0
+
+	for city := 1; city < len(distance); city++ {
+		remainingDistance += fuel[city-1]*mpg - distance[city-1]
+
+		if remainingDistance < minDistance {
+			minDistance = remainingDistance
+			resCity = city
+		}
+	}
+	return resCity
 }
 
 func main() {
