52fe1d463f63b99ddef6b19270c3d4e3fa40bd2c
AntonSakharovAE <anton@alefeducation.com>
2024-03-29 09:01:20 +0400

    impl. zig zag traverse

--- cmd/arrays/zig_zag_traverse/zig_zag_traverse.go
+++ cmd/arrays/zig_zag_traverse/zig_zag_traverse.go
@@ -2,8 +2,50 @@ package main
 
 import "fmt"
 
+// O(w * h) time | O(w * h) space
 func zigZagTraverse(matrix [][]int) []int {
-	return []int{}
+	if matrix == nil || len(matrix) == 0 {
+		return []int{}
+	}
+
+	res := make([]int, 0)
+
+	row, col := 0, 0
+	height := len(matrix) - 1
+	width := len(matrix[height]) - 1
+
+	isGoingDown := true
+
+	for row <= height && col <= width {
+		res = append(res, matrix[row][col])
+
+		if isGoingDown {
+			if row == height || col == 0 {
+				isGoingDown = false
+				if row == height {
+					col++
+				} else {
+					row++
+				}
+			} else {
+				row++
+				col--
+			}
+		} else {
+			if row == 0 || col == width {
+				isGoingDown = true
+				if col == width {
+					row++
+				} else {
+					col++
+				}
+			} else {
+				row--
+				col++
+			}
+		}
+	}
+	return res
 }
 
 func main() {
