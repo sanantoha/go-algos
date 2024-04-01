9c2cabf4374b0bdeeb9bce912cb4d75caafd6d03
AntonSakharovAE <anton@alefeducation.com>
2024-04-01 08:59:52 +0400

    impl. str. permutation and add combination sum.

---
+++ cmd/backtracking/combination_sum/combination_sum.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func combinationSum(arr []int, target int) [][]int {
+	return nil
+}
+
+func main() {
+
+	arr := []int{2, 3, 5, 7}
+
+	fmt.Println(combinationSum(arr, 7)) // // [[2, 2, 3], [2, 5], [7]]
+}
--- cmd/backtracking/permutation/string_permutations/string_permutations.go
+++ cmd/backtracking/permutation/string_permutations/string_permutations.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
+// O(n! * n ^ 2) time | O(n * n!) space
 func permute(str string) []string {
-	return []string{}
+	res := make([]string, 0)
+	backtrack(str, "", &res)
+	return res
+}
+
+func backtrack(str string, ans string, res *[]string) {
+	if len(str) == 0 {
+		*res = append(*res, ans)
+		return
+	}
+
+	for i := 0; i < len(str); i++ {
+		rem := str[:i] + str[i+1:]
+		backtrack(rem, ans+string(str[i]), res)
+	}
 }
 
 func main() {
