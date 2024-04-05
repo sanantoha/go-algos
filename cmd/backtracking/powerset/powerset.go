af9ca17acfc238dcf34cc2ba9e2c31a1bf216e80
AntonSakharovAE <anton@alefeducation.com>
2024-04-05 09:16:08 +0400

    add impl. powerset.

--- cmd/backtracking/powerset/powerset.go
+++ cmd/backtracking/powerset/powerset.go
@@ -2,12 +2,53 @@ package main
 
 import "fmt"
 
+// O(n * 2 ^ n) time | O(n * 2 ^ n) space
 func powerset(arr []int) [][]int {
-	return nil
+
+	res := make([][]int, 0)
+	res = append(res, make([]int, 0))
+
+	for _, num := range arr {
+		subRes := make([][]int, 0)
+		for _, lst := range res {
+			nlst := make([]int, len(lst))
+			copy(nlst, lst)
+			nlst = append(nlst, num)
+			subRes = append(subRes, nlst)
+		}
+
+		for _, sr := range subRes {
+			res = append(res, sr)
+		}
+	}
+
+	return res
 }
 
+// O(n * 2 ^ n) time | O(n * 2 ^ n) space
 func powersetRec(arr []int) [][]int {
-	return nil
+	return helper(arr, len(arr)-1)
+}
+
+func helper(arr []int, idx int) [][]int {
+	if idx == -1 {
+		res := make([][]int, 0)
+		res = append(res, make([]int, 0))
+		return res
+	}
+
+	subRes := helper(arr, idx-1)
+	num := arr[idx]
+	l := len(subRes)
+
+	for i := 0; i < l; i++ {
+		lst := subRes[i]
+		nlst := make([]int, len(lst))
+		copy(nlst, lst)
+		nlst = append(nlst, num)
+		subRes = append(subRes, nlst)
+	}
+	return subRes
 }
 
 func main() {
