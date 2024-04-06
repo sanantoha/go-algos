dcfe9d6102eb946339f8a4b7d2f6d5dfcdd0049c
AntonSakharovAE <anton@alefeducation.com>
2024-04-06 10:43:23 +0400

    add impl. unique_path_iii.

--- cmd/backtracking/unique_path_iii/unique_path_iii.go
+++ cmd/backtracking/unique_path_iii/unique_path_iii.go
@@ -20,8 +20,53 @@ import "fmt"
  * 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
  * 2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
  */
+
+// O(3 ^ n) time | O(n) space
 func uniquePath3(grid [][]int) int {
-	return -1
+	if grid == nil || len(grid) == 0 {
+		return 0
+	}
+
+	zeros := 0
+	startRow := 0
+	startCol := 0
+
+	for row := 0; row < len(grid); row++ {
+		for col := 0; col < len(grid[row]); col++ {
+			if grid[row][col] == 0 {
+				zeros++
+			} else if grid[row][col] == 1 {
+				startRow = row
+				startCol = col
+			}
+		}
+	}
+
+	return paths(grid, startRow, startCol, zeros)
+}
+
+func paths(grid [][]int, row, col, zeros int) int {
+	if row < 0 || col < 0 || row >= len(grid) || col >= len(grid[row]) || grid[row][col] == -1 {
+		return 0
+	}
+	if grid[row][col] == 2 {
+		if zeros == -1 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	zeros--
+	tmp := grid[row][col]
+	grid[row][col] = -1
+
+	count := paths(grid, row-1, col, zeros) + paths(grid, row, col-1, zeros) + paths(grid, row+1, col, zeros) +
+		paths(grid, row, col+1, zeros)
+
+	grid[row][col] = tmp
+
+	return count
 }
 
 func main() {
