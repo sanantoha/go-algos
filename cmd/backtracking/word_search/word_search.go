833799b2b2224cffe2bf73c02eda290913fbc345
AntonSakharovAE <anton@alefeducation.com>
2024-04-07 12:18:41 +0400

    impl. word search.

--- cmd/backtracking/word_search/word_search.go
+++ cmd/backtracking/word_search/word_search.go
@@ -2,8 +2,38 @@ package main
 
 import "fmt"
 
+// O(n * 3 ^ l) time | O(l) space - where n total amount of cells, and l length of word
 func exist(board [][]rune, word string) bool {
+	if board == nil || len(board) == 0 || word == "" {
+		return false
+	}
+
+	for row := 0; row < len(board); row++ {
+		for col := 0; col < len(board[row]); col++ {
+			if board[row][col] == rune(word[0]) && dfs(board, word, row, col, 0) {
+				return true
+			}
+		}
+	}
 	return false
+}
+
+func dfs(board [][]rune, word string, row int, col int, idx int) bool {
+	if idx == len(word) {
+		return true
+	}
+	if row < 0 || col < 0 || row >= len(board) || col >= len(board[row]) || board[row][col] != rune(word[idx]) {
+		return false
+	}
+
+	tmp := board[row][col]
+	board[row][col] = ' '
+
+	found := dfs(board, word, row-1, col, idx+1) || dfs(board, word, row, col-1, idx+1) ||
+		dfs(board, word, row+1, col, idx+1) || dfs(board, word, row, col+1, idx+1)
+
+	board[row][col] = tmp
+	return found
 }
 
 func main() {
