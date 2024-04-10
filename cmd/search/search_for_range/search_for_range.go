c4bf82a522bee45f29038a134600a8e7e5e68b73
AntonSakharovAE <anton@alefeducation.com>
2024-04-10 11:56:16 +0400

    impl. search for range.

--- cmd/search/search_for_range/search_for_range.go
+++ cmd/search/search_for_range/search_for_range.go
@@ -2,8 +2,49 @@ package main
 
 import "fmt"
 
-func searchRange(arr []int, target int) [2]int {
-	return [2]int{}
+// O(log(n)) time | O(1) space
+func searchRange(arr []int, target int) []int {
+	l := leftBinarySearch(arr, target)
+
+	if l < 0 {
+		return []int{}
+	}
+	r := rightBinarySearch(arr, l, target)
+	return []int{l, r}
+}
+
+func rightBinarySearch(arr []int, l int, target int) int {
+	r := len(arr) - 1
+
+	for l <= r {
+		mid := l + (r-l)/2
+		if target >= arr[mid] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return r
+}
+
+func leftBinarySearch(arr []int, target int) int {
+	l := 0
+	r := len(arr) - 1
+
+	for l <= r {
+		mid := l + (r-l)/2
+		if target <= arr[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+
+	if len(arr) > 0 && arr[l] == target {
+		return l
+	} else {
+		return -(l + 1)
+	}
 }
 
 func main() {
