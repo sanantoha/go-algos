8236ae72c12913e06ab2e37b52e4a79e5c3ed0c4
AntonSakharovAE <anton@alefeducation.com>
2024-04-13 11:04:56 +0400

    impl. one edit.

--- cmd/strings/one_edit/one_edit.go
+++ cmd/strings/one_edit/one_edit.go
@@ -1,10 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+// O(n) time | O(1) space - where n is the length of s1 or s2 because should differentiate in 1 character.
 func oneEdit(s1, s2 string) bool {
+	if math.Abs(float64(len(s1)-len(s2))) > 1 {
+		return false
+	}
 
-	return false
+	isOneEdit := false
+
+	i, j := 0, 0
+
+	for i < len(s1) && j < len(s2) {
+		if s1[i] != s2[j] {
+			if isOneEdit {
+				return false
+			}
+			isOneEdit = true
+
+			if len(s1) < len(s2) {
+				i++
+			} else if len(s1) > len(s2) {
+				j++
+			} else {
+				i++
+				j++
+			}
+		} else {
+			i++
+			j++
+		}
+	}
+	return isOneEdit
 }
 
 func main() {
