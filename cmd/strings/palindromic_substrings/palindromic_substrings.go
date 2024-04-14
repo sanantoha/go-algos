6f57c8585f73ab1f0ef9c51bdea2093f940c0677
AntonSakharovAE <anton@alefeducation.com>
2024-04-14 11:48:13 +0400

    impl. palindromic substring.

--- cmd/strings/palindromic_substrings/palindromic_substrings.go
+++ cmd/strings/palindromic_substrings/palindromic_substrings.go
@@ -2,8 +2,33 @@ package main
 
 import "fmt"
 
+// O(n ^ 2) time | O(1) space
 func countSubstrings(str string) int {
-	return -1
+	if str == "" {
+		return 0
+	}
+
+	count := 0
+
+	for i := 0; i < len(str); i++ {
+		count += countPalindrome(str, i, i)
+		count += countPalindrome(str, i, i+1)
+	}
+	return count
+}
+
+func countPalindrome(str string, l, r int) int {
+	count := 0
+	for l >= 0 && r < len(str) {
+		if str[l] != str[r] {
+			break
+		}
+		count++
+		l--
+		r++
+	}
+
+	return count
 }
 
 func main() {
