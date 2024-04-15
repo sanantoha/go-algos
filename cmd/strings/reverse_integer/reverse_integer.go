aeb4892dccaac8408a1bb366127dc53c7d68fe7e
AntonSakharovAE <anton@alefeducation.com>
2024-04-15 08:36:03 +0400

    impl. reverse integer.

--- cmd/strings/reverse_integer/reverse_integer.go
+++ cmd/strings/reverse_integer/reverse_integer.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+// O(1) time | O(1) space
 func reverse(n int) int {
-	return -1
+
+	var res int64 = 0
+
+	for n != 0 {
+		res = res*10 + int64(n)%10
+		if res < math.MinInt || res > math.MaxInt {
+			return 0
+		}
+		n /= 10
+	}
+	return int(res)
 }
 
 func main() {
