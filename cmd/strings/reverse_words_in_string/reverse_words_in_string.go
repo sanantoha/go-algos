62f9f245bbee7c606577fd482f492a3b255792d3
AntonSakharovAE <anton@alefeducation.com>
2024-04-17 09:04:36 +0400

    impl. reverse words

--- cmd/strings/reverse_words_in_string/reverse_words_in_string.go
+++ cmd/strings/reverse_words_in_string/reverse_words_in_string.go
@@ -2,8 +2,33 @@ package main
 
 import "fmt"
 
+// O(n) time | O(n) space
 func reverse(str string) string {
-	return ""
+	arr := []rune(str)
+	rev(arr, 0, len(arr)-1)
+
+	startIdx := 0
+
+	for startIdx < len(arr) {
+		endIdx := startIdx
+
+		for endIdx < len(arr) && arr[endIdx] != ' ' {
+			endIdx++
+		}
+		rev(arr, startIdx, endIdx-1)
+		startIdx = endIdx + 1
+	}
+
+	return string(arr)
+}
+
+func rev(arr []rune, l, r int) {
+
+	for l < r {
+		arr[l], arr[r] = arr[r], arr[l]
+		l++
+		r--
+	}
 }
 
 func main() {
@@ -12,7 +37,7 @@ func main() {
 
 	fmt.Println(reverse(s))
 
-	fmt.Printf("|%s|", reverse(" a good   example     "))
+	fmt.Printf("|%s|\n", reverse(" a good   example     "))
 
 	fmt.Println(reverse("this      string     has a     lot of   whitespace"))
 }
