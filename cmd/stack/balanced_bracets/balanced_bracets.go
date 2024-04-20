2c36b97ce5e3c0abeaf74502af7258e4bc741a9e
AntonSakharovAE <anton@alefeducation.com>
2024-04-20 09:46:52 +0400

    impl. balanced_bracets.go

--- cmd/stack/balanced_bracets/balanced_bracets.go
+++ cmd/stack/balanced_bracets/balanced_bracets.go
@@ -2,8 +2,36 @@ package main
 
 import "fmt"
 
+// O(n) time | O(n) space
 func balancedBrackets(str string) bool {
-	return false
+
+	stack := make([]rune, 0)
+
+	openCloseMap := make(map[rune]rune, 3)
+	openCloseMap['('] = ')'
+	openCloseMap['{'] = '}'
+	openCloseMap['['] = ']'
+
+	closeMap := make(map[rune]bool, 3)
+	closeMap[')'] = true
+	closeMap['}'] = true
+	closeMap[']'] = true
+
+	for _, c := range str {
+		if val, exists := openCloseMap[c]; exists {
+			stack = append(stack, val)
+		} else if _, exists := closeMap[c]; exists {
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if top != c {
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
 }
 
 func main() {
