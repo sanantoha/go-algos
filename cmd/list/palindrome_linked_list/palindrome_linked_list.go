5c1a0a3d1e17caeb281eeca06cc646f3d58e9f4c
AntonSakharovAE <anton@alefeducation.com>
2024-05-01 09:02:43 +0400

    impl. isPalindrome

--- cmd/list/palindrome_linked_list/palindrome_linked_list.go
+++ cmd/list/palindrome_linked_list/palindrome_linked_list.go
@@ -5,8 +5,50 @@ import (
 	"github.com/sanantoha/go-algos/internals/list"
 )
 
+// O(n) time | O(1) space
 func isPalindrome(head *list.ListNode) bool {
-	return false
+	if head == nil || head.Next == nil {
+		return true
+	}
+
+	fst := head
+	snd := reverse(getMiddle(head))
+
+	for snd != nil {
+		if fst.Val != snd.Val {
+			return false
+		}
+		fst = fst.Next
+		snd = snd.Next
+	}
+	return true
+}
+
+func getMiddle(head *list.ListNode) *list.ListNode {
+
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
+
+func reverse(head *list.ListNode) *list.ListNode {
+	curr := head
+	var prev *list.ListNode
+
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+
+	return prev
 }
 
 func main() {
