f25e172682789469b302867e41ce5763722b6403
AntonSakharovAE <anton@alefeducation.com>
2024-06-03 08:51:13 +0400

    1 impl. + 1 add task.

---
+++ cmd/dynamic/levenshtein_distance/levenshtein_distance.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func levenshteinDistance(str1, str2 string) int {
+	return -1
+}
+
+func levenshteinDistance1(str1, str2 string) int {
+	return -1
+}
+
+func main() {
+
+	fmt.Println(levenshteinDistance("abc", "yabd") == 2)
+	fmt.Println(levenshteinDistance1("abc", "yabd") == 2)
+}
--- cmd/tree/same_tree/same_tree.go
+++ cmd/tree/same_tree/same_tree.go
@@ -5,12 +5,56 @@ import (
 	"github.com/sanantoha/go-algos/internals/tree"
 )
 
+// O(n) time | O(h) space
 func isSameTree(t1, t2 *tree.TreeNode) bool {
-	return false
+	if t1 == nil && t2 == nil {
+		return true
+	}
+	if t1 == nil || t2 == nil {
+		return false
+	}
+	if t1.Val != t2.Val {
+		return false
+	}
+	return isSameTree(t1.Left, t2.Left) && isSameTree(t1.Right, t2.Right)
 }
 
+// O(n) time | O(h) space
 func isSameTreeIter(t1, t2 *tree.TreeNode) bool {
-	return false
+	if t1 == nil && t2 == nil {
+		return true
+	}
+	if t1 == nil || t2 == nil {
+		return false
+	}
+
+	stack := make([]*tree.TreeNode, 2)
+	stack[0] = t1
+	stack[1] = t2
+
+	for len(stack) > 0 {
+		curr1 := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		curr2 := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if curr1 == nil && curr2 == nil {
+			continue
+		}
+		if curr1 == nil || curr2 == nil {
+			return false
+		}
+		if curr1.Val != curr2.Val {
+			return false
+		}
+
+		stack = append(stack, curr1.Left)
+		stack = append(stack, curr2.Left)
+		stack = append(stack, curr1.Right)
+		stack = append(stack, curr2.Right)
+	}
+
+	return true
 }
 
 func main() {
