909aadaa76c4876dc655f628d67787992f75242f
AntonSakharovAE <anton@alefeducation.com>
2024-06-18 11:43:04 +0400

    1 impl. + 1 add task. (fix max function)

--- cmd/design/lru_cache/lru_cache.go
+++ cmd/design/lru_cache/lru_cache.go
@@ -1,5 +1,55 @@
 package main
 
+import "fmt"
+
+type LRUCache struct {
+	capacity int
+	cache    map[int]*Node
+	head     *Node
+	tail     *Node
+}
+
+type Node struct {
+	key   int
+	value int
+	next  *Node
+	prev  *Node
+}
+
+func NewLRUCache(capacity int) *LRUCache {
+	head := &Node{}
+	tail := &Node{}
+	head.next = tail
+	tail.prev = head
+
+	return &LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]*Node, capacity),
+		head:     head,
+		tail:     tail,
+	}
+}
+
+func (lc *LRUCache) put(key, value int) {
+
+}
+
+func (lc *LRUCache) get(key int) int {
+	return -1
+}
+
 func main() {
 
+	lruCache := NewLRUCache(2)
+
+	lruCache.put(1, 1)           // cache is {1=1}
+	lruCache.put(2, 2)           // cache is {1=1, 2=2}
+	fmt.Println(lruCache.get(1)) // return 1
+
+	lruCache.put(3, 3)           // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+	fmt.Println(lruCache.get(2)) // returns -1 (not found)
+	lruCache.put(4, 4)           // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+	fmt.Println(lruCache.get(1)) // return -1 (not found)
+	fmt.Println(lruCache.get(3)) // return 3
+	fmt.Println(lruCache.get(4)) // return 4
 }
--- cmd/dynamic/longest_nondecr_subseq/longest_nondecr_subseq.go
+++ cmd/dynamic/longest_nondecr_subseq/longest_nondecr_subseq.go
@@ -1,13 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+// O(n ^ 2) time | O(n) space
 func lnds(arr []int) int {
-	return -1
+	if arr == nil || len(arr) == 0 {
+		return 0
+	}
+
+	lnds := make([]int, len(arr))
+	for i, _ := range arr {
+		lnds[i] = 1
+	}
+
+	maxVal := math.MinInt
+
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] <= arr[i] && lnds[i] < lnds[j]+1 {
+				lnds[i] = lnds[j] + 1
+			}
+		}
+		if maxVal < lnds[i] {
+			maxVal = lnds[i]
+		}
+	}
+	return maxVal
 }
 
+// O(n * log(n)) time | O(n) space
 func lnds1(arr []int) int {
-	return -1
+	if arr == nil || len(arr) == 0 {
+		return 0
+	}
+
+	res := make([]int, 1)
+	res[0] = arr[0]
+
+	for i := 1; i < len(arr); i++ {
+		prev := res[len(res)-1]
+		if prev <= arr[i] {
+			res = append(res, arr[i])
+		} else {
+			j := binarySearch(res, arr[i])
+			if j < 0 {
+				j = -(j + 1)
+			}
+			res[j] = arr[i]
+		}
+	}
+	return len(res)
+}
+
+func binarySearch(arr []int, target int) int {
+	l := 0
+	r := len(arr) - 1
+
+	for l <= r {
+		mid := int(uint(l+r) >> 1)
+		if target < arr[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+
+	if arr[l] == target {
+		return l
+	} else {
+		return -(l + 1)
+	}
 }
 
 func lndsList(arr []int) []int {
