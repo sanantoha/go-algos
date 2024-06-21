06c7bda40eac2773933465d10ea01b6f6ee3b1c4
AntonSakharovAE <anton@alefeducation.com>
2024-06-21 11:36:20 +0400

    1 impl.

--- cmd/dynamic/max_sum_increasing_subsequence/max_sum_increasing_subsequence.go
+++ cmd/dynamic/max_sum_increasing_subsequence/max_sum_increasing_subsequence.go
@@ -3,10 +3,53 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"slices"
 )
 
+// O(n ^ 2) time | O(n) space
 func maxSumIncreasingSubsequence(arr []int) [][]int {
-	return nil
+	if arr == nil || len(arr) == 0 {
+		return nil
+	}
+
+	msi := make([]int, len(arr))
+	prev := make([]int, len(arr))
+
+	for i, v := range arr {
+		msi[i] = v
+		prev[i] = -1
+	}
+
+	maxIdx := 0
+
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] < arr[i] && msi[i] < msi[j]+arr[i] {
+				msi[i] = msi[j] + arr[i]
+				prev[i] = j
+			}
+		}
+		if msi[maxIdx] < msi[i] {
+			maxIdx = i
+		}
+	}
+	return [][]int{
+		{msi[maxIdx]},
+		buildSeq(arr, prev, maxIdx),
+	}
+}
+
+func buildSeq(arr []int, prev []int, maxIdx int) []int {
+	idx := maxIdx
+	res := make([]int, 0)
+
+	for idx >= 0 {
+		res = append(res, arr[idx])
+		idx = prev[idx]
+	}
+
+	slices.Reverse(res)
+	return res
 }
 
 func main() {
