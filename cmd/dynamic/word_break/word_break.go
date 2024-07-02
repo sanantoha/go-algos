fd635788164aa70c888a04176b42bef57d450fd5
AntonSakharovAE <anton@alefeducation.com>
2024-07-02 08:58:35 +0400

    1 impl. + 1 new tasks.

--- cmd/dynamic/word_break/word_break.go
+++ cmd/dynamic/word_break/word_break.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// O(n ^ 3) time | O(n) space
 func wordBreak(s string, wordDict []string) bool {
-	return false
+	if s == "" || wordDict == nil || len(wordDict) == 0 {
+		return false
+	}
+
+	wordSet := make(map[string]struct{})
+	for _, word := range wordDict {
+		wordSet[word] = struct{}{}
+	}
+
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			w := s[j:i]
+			if dp[j] {
+				if _, exist := wordSet[w]; exist {
+					dp[i] = true
+					break
+				}
+			}
+		}
+	}
+
+	return dp[len(s)]
 }
 
 func main() {
---
+++ cmd/sorting/sort_k_sorted_array/sort_k_sorted_array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func sortKSortedArray(arr []int, k int) []int {
+	return nil
+}
+
+func main() {
+
+	arr := []int{3, 2, 1, 5, 4, 7, 6, 5}
+
+	fmt.Println(sortKSortedArray(arr, 3))
+}
