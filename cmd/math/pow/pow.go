e2ce1a4832718031a59328e016f1aa5230cad488
AntonSakharovAE <anton@alefeducation.com>
2024-07-05 17:50:54 +0400

    1 impl.

--- cmd/math/pow/pow.go
+++ cmd/math/pow/pow.go
@@ -5,8 +5,20 @@ import (
 	"math"
 )
 
+// O(log(n)) time | O(log(n)) space
 func pow(x float64, n int) float64 {
-	return -1
+	if n == 0 {
+		return 1
+	} else if n == 1 {
+		return x
+	} else if n < 0 {
+		return pow(1/x, -n)
+	} else if n%2 == 0 {
+		y := pow(x, n/2)
+		return y * y
+	} else {
+		return x * pow(x, n-1)
+	}
 }
 
 func main() {
