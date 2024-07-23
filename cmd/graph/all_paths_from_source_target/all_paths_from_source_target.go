ec0e25d3c221272c5dbc54684a72d4ebacc6512a
AntonSakharovAE <anton@alefeducation.com>
2024-07-23 08:40:25 +0400

    add 1 impl.

--- cmd/graph/all_paths_from_source_target/all_paths_from_source_target.go
+++ cmd/graph/all_paths_from_source_target/all_paths_from_source_target.go
@@ -27,8 +27,41 @@ func backtrack(graph [][]int, v int, ans *[]int, res *[][]int) {
 	}
 }
 
+// O(V ^ 2 * 2 ^ V) time | O(2 ^ V) space
 func allPathsSourceTarget(graph [][]int) [][]int {
-	return nil
+
+	path := make([]int, 1)
+	path[0] = 0
+
+	stack := make([]Info, 1)
+	stack[0] = Info{
+		v:    0,
+		path: path,
+	}
+
+	res := make([][]int, 0)
+
+	for len(stack) > 0 {
+		info := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if len(graph)-1 == info.v {
+			res = append(res, info.path)
+		}
+
+		for _, u := range graph[info.v] {
+			p := make([]int, len(info.path))
+			copy(p, info.path)
+			p = append(p, u)
+			stack = append(stack, Info{u, p})
+		}
+	}
+	return res
+}
+
+type Info struct {
+	v    int
+	path []int
 }
 
 func main() {
@@ -43,8 +76,8 @@ func main() {
 	actualRec := allPathsSourceTargetRec(graph)
 	actual := allPathsSourceTarget(graph)
 
-	fmt.Println(actualRec) // [[0, 1, 3], [0, 2, 3]]
-	fmt.Println(actual)    // [[0, 1, 3], [0, 2, 3]]
+	fmt.Println(actualRec) // [[0 1 3] [0 2 3]]
+	fmt.Println(actual)    // [[0 2 3] [0 1 3]]
 
 	graph1 := [][]int{
 		{4, 3, 1},
@@ -57,6 +90,6 @@ func main() {
 	actualRec = allPathsSourceTargetRec(graph1)
 	actual = allPathsSourceTarget(graph1)
 
-	fmt.Println(actualRec) // [[0,4], [0,3,4], [0,1,3,4], [0,1,2,3,4], [0,1,4]]
-	fmt.Println(actual)    // [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]]
+	fmt.Println(actualRec) // [[0 4] [0 3 4] [0 1 3 4] [0 1 2 3 4] [0 1 4]]
+	fmt.Println(actual)    // [[0 1 4] [0 1 2 3 4] [0 1 3 4] [0 3 4] [0 4]]
 }
