1a58b47b6017504a4e580cd392aa7a4a9ba53ab5
AntonSakharovAE <anton@alefeducation.com>
2024-07-24 08:12:06 +0400

    add 1 impl.

--- cmd/graph/bellman_ford/bellman_ford.go
+++ cmd/graph/bellman_ford/bellman_ford.go
@@ -4,10 +4,37 @@ import (
 	"fmt"
 	grph "github.com/sanantoha/go-algos/internals/graph"
 	log "github.com/sirupsen/logrus"
+	"math"
 )
 
+// O(E * V) time | O(V) space
 func findShortestPath(graph *grph.EdgeWeightedDigraph, start int) *grph.ShortestPath {
-	return nil
+	shortest := make([]float64, graph.V)
+	prev := make([]int, graph.V)
+	for i := 0; i < graph.V; i++ {
+		shortest[i] = math.MaxFloat64
+		prev[i] = -1
+	}
+	shortest[start] = 0
+
+	for v := 0; v < graph.V-1; v++ {
+		for _, edge := range graph.Edges() {
+			relax(shortest, prev, edge)
+		}
+	}
+
+	return &grph.ShortestPath{
+		Shortest: shortest,
+		Prev:     prev,
+	}
+}
+
+func relax(shortest []float64, prev []int, edge *grph.DirectedEdge) {
+	newWeight := shortest[edge.From()] + edge.Weight
+	if newWeight < shortest[edge.To()] {
+		shortest[edge.To()] = newWeight
+		prev[edge.To()] = edge.From()
+	}
 }
 
 func findNegativeWeightCycle(graph *grph.EdgeWeightedDigraph, path *grph.ShortestPath) []int {
