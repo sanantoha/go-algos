68507a600608a7f2cdd7bdfa883bf13936785d08
AntonSakharovAE <anton@alefeducation.com>
2024-08-12 08:41:05 +0400

    1impl

--- cmd/graph/number_of_island/number_of_island.go
+++ cmd/graph/number_of_island/number_of_island.go
@@ -2,8 +2,62 @@ package main
 
 import "fmt"
 
+// O(w * h) time | O(w * h) space
 func numberOfIsland(grid [][]int) int {
-	return -1
+	if len(grid) == 0 {
+		return 0
+	}
+
+	number := 0
+
+	for row := 0; row < len(grid); row++ {
+		for col := 0; col < len(grid[row]); col++ {
+			if grid[row][col] == 1 {
+				dfs(grid, row, col)
+				number++
+			}
+		}
+	}
+
+	return number
+}
+
+func dfs(grid [][]int, startRow int, startCol int) {
+	stack := make([][]int, 1)
+	stack[0] = []int{startRow, startCol}
+
+	for len(stack) > 0 {
+		p := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		row := p[0]
+		col := p[1]
+
+		if grid[row][col] != 1 {
+			continue
+		}
+		grid[row][col] = -1
+
+		stack = append(stack, getNeighbors(grid, row, col)...)
+	}
+}
+
+func getNeighbors(grid [][]int, row int, col int) [][]int {
+	res := make([][]int, 0)
+
+	if row > 0 {
+		res = append(res, []int{row - 1, col})
+	}
+	if col > 0 {
+		res = append(res, []int{row, col - 1})
+	}
+	if row+1 < len(grid) {
+		res = append(res, []int{row + 1, col})
+	}
+	if col+1 < len(grid[row]) {
+		res = append(res, []int{row, col + 1})
+	}
+
+	return res
 }
 
 func main() {
