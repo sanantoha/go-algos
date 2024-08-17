939f15cdcbcf69d658cd57902934e61de1081fcd
AntonSakharovAE <anton@alefeducation.com>
2024-08-17 09:42:44 +0400

    2impl

--- cmd/graph/word_ladder/word_ladder.go
+++ cmd/graph/word_ladder/word_ladder.go
@@ -2,8 +2,76 @@ package main
 
 import "fmt"
 
+// O(n * l ^ 2) time | O(n * l ^ 2) space
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-	return -1
+	if beginWord == "" || endWord == "" {
+		return 0
+	}
+	endWordExists := false
+	for _, v := range wordList {
+		if v == endWord {
+			endWordExists = true
+			break
+		}
+	}
+
+	if !endWordExists {
+		return 0
+	}
+
+	adjList := createAdjList(wordList)
+
+	queue := make([]*Node, 1)
+	queue[0] = &Node{beginWord, 1}
+
+	visited := make(map[string]bool, 0)
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		if endWord == node.word {
+			return node.level
+		}
+
+		if visited[node.word] {
+			continue
+		}
+		visited[node.word] = true
+
+		for i := 0; i < len(node.word); i++ {
+			key := node.word[:i] + "*" + node.word[i+1:]
+			for _, word := range adjList[key] {
+
+				queue = append(queue, &Node{word, node.level + 1})
+			}
+		}
+
+	}
+
+	return 0
+}
+
+type Node struct {
+	word  string
+	level int
+}
+
+func createAdjList(wordList []string) map[string][]string {
+	adjList := make(map[string][]string, 0)
+
+	for _, word := range wordList {
+		for i := 0; i < len(word); i++ {
+			key := word[:i] + "*" + word[i+1:]
+			lst, ok := adjList[key]
+			if !ok {
+				lst = make([]string, 0)
+			}
+			lst = append(lst, word)
+			adjList[key] = lst
+		}
+	}
+	return adjList
 }
 
 /**
--- cmd/graph/word_ladderII/word_ladderII.go
+++ cmd/graph/word_ladderII/word_ladderII.go
@@ -2,8 +2,98 @@ package main
 
 import "fmt"
 
+// O(n * l ^ 2) time | O(n * l ^ 2) space
 func findLadders(beginWord string, endWord string, wordList []string) [][]string {
-	return nil
+	if beginWord == "" || endWord == "" {
+		return nil
+	}
+	endWordExists := false
+	for _, v := range wordList {
+		if v == endWord {
+			endWordExists = true
+			break
+		}
+	}
+
+	if !endWordExists {
+		return nil
+	}
+
+	res := make([][]string, 0)
+
+	queue := make([]*Node, 1)
+	initPath := make([]string, 1)
+	initPath[0] = beginWord
+	queue[0] = &Node{
+		word: beginWord,
+		path: initPath,
+	}
+
+	adjList := createAdjList(wordList)
+
+	visited := make(map[string]bool, 0)
+
+	for len(queue) > 0 {
+		size := len(queue)
+
+		currVisited := make(map[string]bool, 0)
+
+		for size > 0 {
+			size--
+
+			node := queue[0]
+			queue = queue[1:]
+
+			if endWord == node.word {
+				res = append(res, node.path)
+				continue
+			}
+
+			for i := 0; i < len(node.word); i++ {
+				key := node.word[:i] + "*" + node.word[i+1:]
+
+				for _, word := range adjList[key] {
+					if visited[word] {
+						continue
+					}
+					currVisited[word] = true
+					path := make([]string, len(node.path))
+					copy(path, node.path)
+					path = append(path, word)
+
+					queue = append(queue, &Node{word, path})
+				}
+			}
+		}
+
+		for key, value := range currVisited {
+			visited[key] = value
+		}
+	}
+
+	return res
+}
+
+type Node struct {
+	word string
+	path []string
+}
+
+func createAdjList(wordList []string) map[string][]string {
+	adjList := make(map[string][]string, 0)
+
+	for _, word := range wordList {
+		for i := 0; i < len(word); i++ {
+			key := word[:i] + "*" + word[i+1:]
+			lst, ok := adjList[key]
+			if !ok {
+				lst = make([]string, 0)
+			}
+			lst = append(lst, word)
+			adjList[key] = lst
+		}
+	}
+	return adjList
 }
 
 func main() {
