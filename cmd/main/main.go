23d195f3d8b3045b34af46aa0e7f2f08d071ec86
AntonSakharovAE <anton@alefeducation.com>
2025-08-05 07:44:18 +0400

    +1 task completed + add tests

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/sanantoha/go-algos/internals/tree"
+	grph "github.com/sanantoha/go-algos/internals/graph"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -31,7 +31,8 @@ func main() {
 	}
 
 	processedTasks := map[string]bool{
-		"symmetric_tree.go": true,
+		"symmetric_tree.go":         true,
+		"prim_min_spanning_tree.go": true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -53,72 +54,58 @@ func main() {
 
 func runTask() {
 
-	root := &tree.TreeNode{
-		Val: 1,
-		Left: &tree.TreeNode{
-			Val: 2,
-			Left: &tree.TreeNode{
-				Val: 3,
-			},
-			Right: &tree.TreeNode{
-				Val: 4,
-			},
-		},
-		Right: &tree.TreeNode{
-			Val: 2,
-			Left: &tree.TreeNode{
-				Val: 4,
-			},
-			Right: &tree.TreeNode{
-				Val: 3,
-			},
-		},
-	}
+	graph := grph.NewEdgeWeightedGraph(6)
 
-	fmt.Println(isSymmetricRec(root))
-	fmt.Println(isSymmetric(root))
+	graph.AddEdge(grph.NewEdge(0, 1, 7.0))
+	graph.AddEdge(grph.NewEdge(0, 2, 8.0))
+	graph.AddEdge(grph.NewEdge(1, 2, 3.0))
+	graph.AddEdge(grph.NewEdge(1, 3, 6.0))
+	graph.AddEdge(grph.NewEdge(2, 3, 4.0))
+	graph.AddEdge(grph.NewEdge(2, 4, 3.0))
+	graph.AddEdge(grph.NewEdge(3, 4, 2.0))
+	graph.AddEdge(grph.NewEdge(3, 5, 5.0))
+	graph.AddEdge(grph.NewEdge(4, 5, 2.0))
+	/*
+	   6 5
+	   0: 0-1 7.00
+	   1: 1-2 3.00  0-1 7.00
+	   2: 1-2 3.00  2-4 3.00
+	   3: 3-4 2.00
+	   4: 3-4 2.00  4-5 2.00  2-4 3.00
+	   5: 4-5 2.00
+	*/
+	fmt.Println(graph)
+	fmt.Println("=========================================")
+	fmt.Println(mst(graph))
+	fmt.Println("=========================================")
 
-	fmt.Println("==================================")
+	graph1 := grph.NewEdgeWeightedGraph(7)
+	graph1.AddEdge(grph.NewEdge(0, 1, 2.0))
+	graph1.AddEdge(grph.NewEdge(0, 2, 3.0))
+	graph1.AddEdge(grph.NewEdge(0, 3, 7.0))
+	graph1.AddEdge(grph.NewEdge(1, 2, 6.0))
+	graph1.AddEdge(grph.NewEdge(1, 6, 3.0))
+	graph1.AddEdge(grph.NewEdge(2, 4, 1.0))
+	graph1.AddEdge(grph.NewEdge(2, 5, 8.0))
+	graph1.AddEdge(grph.NewEdge(3, 4, 5.0))
+	graph1.AddEdge(grph.NewEdge(4, 5, 4.0))
+	graph1.AddEdge(grph.NewEdge(5, 6, 2.0))
 
-	root1 := &tree.TreeNode{
-		Val: 1,
-		Left: &tree.TreeNode{
-			Val: 2,
-			Right: &tree.TreeNode{
-				Val: 3,
-			},
-		},
-		Right: &tree.TreeNode{
-			Val: 2,
-			Right: &tree.TreeNode{
-				Val: 3,
-			},
-		},
-	}
-
-	fmt.Println(!isSymmetricRec(root1))
-	fmt.Println(!isSymmetric(root1))
-
-	fmt.Println("==================================")
-
-	root2 := &tree.TreeNode{
-		Val: 2,
-		Left: &tree.TreeNode{
-			Val: 1,
-		},
-		Right: &tree.TreeNode{
-			Val: 3,
-		},
-	}
-
-	fmt.Println(!isSymmetricRec(root2))
-	fmt.Println(!isSymmetric(root2))
+	/*
+	   7 6
+	   0: 0-1 2.00000  0-2 3.00000
+	   1: 0-1 2.00000  1-6 3.00000
+	   2: 2-4 1.00000  0-2 3.00000
+	   3: 3-4 5.00000
+	   4: 2-4 1.00000  3-4 5.00000
+	   5: 5-6 2.00000
+	   6: 5-6 2.00000  1-6 3.00000
+	*/
+	fmt.Println(graph1)
+	fmt.Println("=========================================")
+	fmt.Println(mst(graph1))
 }
 
-func isSymmetricRec(root *tree.TreeNode) bool {
-	return false
-}
-
-func isSymmetric(root *tree.TreeNode) bool {
-	return false
+func mst(graph *grph.EdgeWeightedGraph) *grph.EdgeWeightedGraph {
+	return nil
 }
