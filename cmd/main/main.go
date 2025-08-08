e87b0079123ba4f237add7efd2d1edb00db69092
AntonSakharovAE <anton@alefeducation.com>
2025-08-08 07:13:03 +0400

    +1 task completed + add tests

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -34,6 +34,7 @@ func main() {
 		"prim_min_spanning_tree.go":                   true,
 		"min_swaps_transform_string_to_palindrome.go": true,
 		"lru_cache.go":                                true,
+		"heap_sort.go":                                true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -55,31 +56,18 @@ func main() {
 
 func runTask() {
 
-	lruCache := NewLRUCache(2)
+	arr := make([]int, 10)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = rand.Intn(50)
+	}
 
-	lruCache.put(1, 1)           // cache is {1=1}
-	lruCache.put(2, 2)           // cache is {1=1, 2=2}
-	fmt.Println(lruCache.get(1)) // return 1
+	fmt.Println(arr)
 
-	lruCache.put(3, 3)           // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-	fmt.Println(lruCache.get(2)) // returns -1 (not found)
-	lruCache.put(4, 4)           // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-	fmt.Println(lruCache.get(1)) // return -1 (not found)
-	fmt.Println(lruCache.get(3)) // return 3
-	fmt.Println(lruCache.get(4)) // return 4
+	heapSort(arr)
+
+	fmt.Println(arr)
 }
 
-type LRUCache struct {
-}
+func heapSort(arr []int) {
 
-func NewLRUCache(capacity int) *LRUCache {
-	return nil
-}
-
-func (lc *LRUCache) put(key int, value int) {
-
-}
-
-func (lc *LRUCache) get(key int) int {
-	return -1
 }
