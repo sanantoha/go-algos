bc41a21a0ee4c869cd1d00c55b59ae3af7f4690b
AntonSakharovAE <anton@alefeducation.com>
2025-08-13 07:08:45 +0400

    +1 task completed + add tests

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/sanantoha/go-algos/internals/tree"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -37,6 +38,7 @@ func main() {
 		"heap_sort.go":                                true,
 		"reverse_list.go":                             true,
 		"levenshtein_distance.go":                     true,
+		"bst_successor_search.go":                     true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -58,14 +60,52 @@ func main() {
 
 func runTask() {
 
-	fmt.Println(levenshteinDistance("abc", "yabd") == 2)
-	fmt.Println(levenshteinDistance1("abc", "yabd") == 2)
+	node5 := &tree.Node{Key: 5}
+	node9 := &tree.Node{Key: 9}
+	node11 := &tree.Node{Key: 11}
+	node12 := &tree.Node{Key: 12}
+	node14 := &tree.Node{Key: 14}
+	node20 := &tree.Node{Key: 20}
+	node25 := &tree.Node{Key: 25}
+
+	node5.Parent = node9
+
+	node9.Left = node5
+	node9.Right = node12
+	node9.Parent = node20
+
+	node11.Parent = node12
+
+	node12.Left = node11
+	node12.Right = node14
+	node12.Parent = node9
+
+	node14.Parent = node12
+
+	node20.Left = node9
+	node20.Right = node25
+
+	node25.Parent = node20
+
+	//         20
+	//       /    \
+	//      9      25
+	//    /   \
+	//   5     12
+	//        /   \
+	//       11   14
+
+	fmt.Println(node20)
+	fmt.Println(findInOrderSuccessor(node5).Key == 9)
+	fmt.Println(findInOrderSuccessor(node9).Key == 11)
+	fmt.Println(findInOrderSuccessor(node11).Key == 12)
+	fmt.Println(findInOrderSuccessor(node12).Key == 14)
+	fmt.Println(findInOrderSuccessor(node14).Key == 20)
+	fmt.Println(findInOrderSuccessor(node20).Key == 25)
+	fmt.Println(findInOrderSuccessor(node25) == nil)
+	fmt.Println(findInOrderSuccessor(node20).Key == 25)
 }
 
-func levenshteinDistance(str1, str2 string) int {
-	return 0
-}
-
-func levenshteinDistance1(str1, str2 string) int {
-	return 0
+func findInOrderSuccessor(root *tree.Node) *tree.Node {
+	return nil
 }
