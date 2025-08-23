bcb419df859d44663918b2cf90466d316b0e0b9f
AntonSakharovAE <anton@alefeducation.com>
2025-08-23 13:16:44 +0400

    +1 task completed + add tests

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/sanantoha/go-algos/internals/tree"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -43,6 +44,7 @@ func main() {
 		"all_elements_in_two_binary_search_trees.go":  true,
 		"subarray_sort.go":                            true,
 		"search_in_rotated_sorted_array.go":           true,
+		"branch_sums.go":                              true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -64,22 +66,45 @@ func main() {
 
 func runTask() {
 
-	arr := []int{40, 50, 60, 70, 80, 90, 0, 10, 20, 30, 31, 32, 33, 34, 35}
-	target := 90
+	root := &tree.TreeNode{
+		Val: 1,
+		Left: &tree.TreeNode{
+			Val: 2,
+			Left: &tree.TreeNode{
+				Val: 4,
+				Left: &tree.TreeNode{
+					Val: 8,
+				},
+				Right: &tree.TreeNode{
+					Val: 9,
+				},
+			},
+			Right: &tree.TreeNode{
+				Val: 5,
+				Left: &tree.TreeNode{
+					Val: 10,
+				},
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 3,
+			Left: &tree.TreeNode{
+				Val: 6,
+			},
+			Right: &tree.TreeNode{
+				Val: 7,
+			},
+		},
+	}
 
-	fmt.Println(findSmallestIdx(arr))
-	fmt.Println(search(arr, target))
-	fmt.Println(search1(arr, target))
+	fmt.Println(branchSums(root))     // [15, 16, 18, 10, 11]
+	fmt.Println(branchSumsIter(root)) // [11, 10, 18, 16, 15]
 }
 
-func findSmallestIdx(arr []int) int {
-	return -1
+func branchSums(root *tree.TreeNode) []int {
+	return nil
 }
 
-func search(arr []int, target int) int {
-	return -1
-}
-
-func search1(arr []int, target int) int {
-	return -1
+func branchSumsIter(root *tree.TreeNode) []int {
+	return nil
 }
