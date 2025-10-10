d23d5092a92e2d9a9ec604a4c1599d27fc215767
AntonSakharovAE <anton@alefeducation.com>
2025-10-10 08:18:48 +0400

    +1 task completed.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,8 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/sanantoha/go-algos/internals/tree"
 )
 
 func main() {
@@ -91,6 +89,7 @@ func main() {
 		"binary_tree_diameter.go":                            true,
 		"greater_common_divisor.go":                          true,
 		"same_tree.go":                                       true,
+		"sparse_matrix_multiplication.go":                    true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -112,107 +111,26 @@ func main() {
 
 func runTask() {
 
-	t1 := &tree.TreeNode{
-		Val: 5,
-		Left: &tree.TreeNode{
-			Val: 2,
-			Left: &tree.TreeNode{
-				Val: 1,
-			},
-			Right: &tree.TreeNode{
-				Val: 3,
-			},
-		},
-		Right: &tree.TreeNode{
-			Val: 10,
-			Left: &tree.TreeNode{
-				Val: 7,
-			},
-			Right: &tree.TreeNode{
-				Val: 15,
-			},
-		},
+	mat1 := [][]int{
+		{1, 0, 0},
+		{-1, 0, 3},
 	}
 
-	t2 := &tree.TreeNode{
-		Val: 5,
-		Left: &tree.TreeNode{
-			Val: 2,
-			Left: &tree.TreeNode{
-				Val: 1,
-			},
-			Right: &tree.TreeNode{
-				Val: 3,
-			},
-		},
-		Right: &tree.TreeNode{
-			Val: 10,
-			Left: &tree.TreeNode{
-				Val: 7,
-			},
-			Right: &tree.TreeNode{
-				Val: 15,
-			},
-		},
+	mat2 := [][]int{
+		{7, 0, 0},
+		{0, 0, 0},
+		{0, 0, 1},
 	}
 
-	fmt.Println(isSameTree(t1, t2))
-	fmt.Println(isSameTreeIter(t1, t2))
-
-	fmt.Println("================================")
-
-	t11 := &tree.TreeNode{
-		Val: 5,
-		Left: &tree.TreeNode{
-			Val: 2,
-			Left: &tree.TreeNode{
-				Val: 1,
-			},
-			Right: &tree.TreeNode{
-				Val: 3,
-			},
-		},
-		Right: &tree.TreeNode{
-			Val: 10,
-			Left: &tree.TreeNode{
-				Val: 7,
-			},
-			Right: &tree.TreeNode{
-				Val: 15,
-			},
-		},
-	}
-
-	t22 := &tree.TreeNode{
-		Val: 5,
-		Left: &tree.TreeNode{
-			Val: 2,
-			Left: &tree.TreeNode{
-				Val: 1,
-			},
-			Right: &tree.TreeNode{
-				Val: 4,
-			},
-		},
-		Right: &tree.TreeNode{
-			Val: 10,
-			Left: &tree.TreeNode{
-				Val: 7,
-			},
-			Right: &tree.TreeNode{
-				Val: 15,
-			},
-		},
-	}
-
-	fmt.Println(!isSameTree(t11, t22))
-	fmt.Println(!isSameTreeIter(t11, t22))
+	fmt.Println(multiply(mat1, mat2)) // [[7, 0, 0], [-7, 0, 3]]
+	fmt.Println("=================")
+	fmt.Println(multiply1(mat1, mat2)) // [[7, 0, 0], [-7, 0, 3]]
 }
 
-func isSameTree(t1, t2 *tree.TreeNode) bool {
-	return false
+func multiply(mat1 [][]int, mat2 [][]int) [][]int {
+	return nil
 }
 
-func isSameTreeIter(t1, t2 *tree.TreeNode) bool {
-	return false
+func multiply1(mat1 [][]int, mat2 [][]int) [][]int {
+	return nil
 }
