d7e4815a56e6d593c82027cbd312891b4ec5e230
sanantoha <sanantoha@gmail.com>
2025-12-24 08:15:18 +0400

    +1 task completed.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/sanantoha/go-algos/internals/tree"
 )
 
 func main() {
@@ -156,6 +158,7 @@ func main() {
 		"kruskal_min_spanning_tree_as_map.go":                true,
 		"staircase_traversal.go":                             true,
 		"minimum_window_substring.go":                        true,
+		"merge_binary_tree.go":                               true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -177,13 +180,103 @@ func main() {
 
 func runTask() {
 
-	fmt.Println(minimumWindowSubstring("ADOBECODEBANC", "ABC")) // "BANC"
+	tree1 := &tree.TreeNode{
+		Val: 1,
+		Left: &tree.TreeNode{
+			Val: 3,
+			Left: &tree.TreeNode{
+				Val: 7,
+			},
+			Right: &tree.TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 2,
+		},
+	}
 
-	fmt.Println(minimumWindowSubstring("a", "a")) // a
+	tree2 := &tree.TreeNode{
+		Val: 1,
+		Left: &tree.TreeNode{
+			Val: 5,
+			Left: &tree.TreeNode{
+				Val: 2,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 9,
+			Left: &tree.TreeNode{
+				Val: 7,
+			},
+			Right: &tree.TreeNode{
+				Val: 6,
+			},
+		},
+	}
 
-	fmt.Println(minimumWindowSubstring("a", "aa")) // ""
+	actual := mergeBinaryTrees(tree1, tree2)
+
+	fmt.Println(actual.Val == 2)
+	fmt.Println(actual.Left.Val == 8)
+	fmt.Println(actual.Left.Left.Val == 9)
+	fmt.Println(actual.Left.Right.Val == 4)
+	fmt.Println(actual.Right.Val == 11)
+	fmt.Println(actual.Right.Left.Val == 7)
+	fmt.Println(actual.Right.Right.Val == 6)
+
+	fmt.Println("==========================================")
+
+	tree3 := &tree.TreeNode{
+		Val: 1,
+		Left: &tree.TreeNode{
+			Val: 3,
+			Left: &tree.TreeNode{
+				Val: 7,
+			},
+			Right: &tree.TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 2,
+		},
+	}
+
+	tree4 := &tree.TreeNode{
+		Val: 1,
+		Left: &tree.TreeNode{
+			Val: 5,
+			Left: &tree.TreeNode{
+				Val: 2,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 9,
+			Left: &tree.TreeNode{
+				Val: 7,
+			},
+			Right: &tree.TreeNode{
+				Val: 6,
+			},
+		},
+	}
+
+	actual = mergeBinaryTreesIter(tree3, tree4)
+
+	fmt.Println(actual.Val == 2)
+	fmt.Println(actual.Left.Val == 8)
+	fmt.Println(actual.Left.Left.Val == 9)
+	fmt.Println(actual.Left.Right.Val == 4)
+	fmt.Println(actual.Right.Val == 11)
+	fmt.Println(actual.Right.Left.Val == 7)
+	fmt.Println(actual.Right.Right.Val == 6)
 }
 
-func minimumWindowSubstring(s string, t string) string {
-	return ""
+func mergeBinaryTrees(tree1, tree2 *tree.TreeNode) *tree.TreeNode {
+	return nil
+}
+
+func mergeBinaryTreesIter(tree1, tree2 *tree.TreeNode) *tree.TreeNode {
+	return nil
 }
