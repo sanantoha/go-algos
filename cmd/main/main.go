4a293c3ed645209e9616d121d486437ad5ca1c23
sanantoha <sanantoha@gmail.com>
2026-01-14 07:57:08 +0400

    +1 task completed.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,8 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-
-	grph "github.com/sanantoha/go-algos/internals/graph"
 )
 
 func main() {
@@ -194,86 +192,27 @@ func main() {
 
 func runTask() {
 
-	graph := createGraph()
-	/*
-	   6 5
-	   0: 0-1 7.00
-	   1: 1-2 3.00  0-1 7.00
-	   2: 1-2 3.00  2-4 3.00
-	   3: 3-4 2.00
-	   4: 3-4 2.00  4-5 2.00  2-4 3.00
-	   5: 4-5 2.00
-	*/
+	fmt.Println(climbStairsDP(0) == 0)
+	fmt.Println(climbStairsDP(1) == 1)
+	fmt.Println(climbStairsDP(2) == 2)
+	fmt.Println(climbStairsDP(3) == 3)
+	fmt.Println(climbStairsDP(4) == 5)
+	fmt.Println(climbStairsDP(5) == 8)
 
-	fmt.Println(grph.PrintGraphAsAdjList(graph))
-	fmt.Println("=========================================")
-	fmt.Println(grph.PrintGraphAsAdjList(mst(graph)))
-	fmt.Println("=========================================")
+	fmt.Println("=======================")
 
-	graph1 := createGraph1()
-
-	/*
-	   7 6
-	   0: 0-1 2.00  0-2 3.00
-	   1: 0-1 2.00  1-6 3.00
-	   2: 2-4 1.00  0-2 3.00
-	   3: 3-4 5.00
-	   4: 2-4 1.00  3-4 5.00
-	   5: 5-6 2.00
-	   6: 5-6 2.00  1-6 3.00
-	*/
-	fmt.Println(grph.PrintGraphAsAdjList(graph1))
-	fmt.Println("=========================================")
-	fmt.Println(grph.PrintGraphAsAdjList(mst(graph1)))
+	fmt.Println(climbStairs(0) == 0)
+	fmt.Println(climbStairs(1) == 1)
+	fmt.Println(climbStairs(2) == 2)
+	fmt.Println(climbStairs(3) == 3)
+	fmt.Println(climbStairs(4) == 5)
+	fmt.Println(climbStairs(5) == 8)
 }
 
-func mst(graph map[string][]*grph.EdgeT[string]) map[string][]*grph.EdgeT[string] {
-	return nil
+func climbStairsDP(n int) int {
+	return 0
 }
 
-func createGraph() map[string][]*grph.EdgeT[string] {
-	graph := make(map[string][]*grph.EdgeT[string], 0)
-
-	edge01 := grph.NewEdgeT("0", "1", 7.0)
-	edge02 := grph.NewEdgeT("0", "2", 8.0)
-	edge12 := grph.NewEdgeT("1", "2", 3.0)
-	edge13 := grph.NewEdgeT("1", "3", 6.0)
-	edge23 := grph.NewEdgeT("2", "3", 4.0)
-	edge24 := grph.NewEdgeT("2", "4", 3.0)
-	edge34 := grph.NewEdgeT("3", "4", 2.0)
-	edge35 := grph.NewEdgeT("3", "5", 5.0)
-	edge45 := grph.NewEdgeT("4", "5", 2.0)
-
-	graph["0"] = []*grph.EdgeT[string]{edge01, edge02}
-	graph["1"] = []*grph.EdgeT[string]{edge01, edge12, edge13}
-	graph["2"] = []*grph.EdgeT[string]{edge02, edge12, edge23, edge24}
-	graph["3"] = []*grph.EdgeT[string]{edge13, edge23, edge34, edge35}
-	graph["4"] = []*grph.EdgeT[string]{edge24, edge34, edge45}
-	graph["5"] = []*grph.EdgeT[string]{edge35, edge45}
-	return graph
-}
-
-func createGraph1() map[string][]*grph.EdgeT[string] {
-	graph := make(map[string][]*grph.EdgeT[string], 0)
-
-	edge01 := grph.NewEdgeT("0", "1", 2.0)
-	edge02 := grph.NewEdgeT("0", "2", 3.0)
-	edge03 := grph.NewEdgeT("0", "3", 7.0)
-	edge12 := grph.NewEdgeT("1", "2", 6.0)
-	edge16 := grph.NewEdgeT("1", "6", 3.0)
-	edge24 := grph.NewEdgeT("2", "4", 1.0)
-	edge25 := grph.NewEdgeT("2", "5", 8.0)
-	edge34 := grph.NewEdgeT("3", "4", 5.0)
-	edge45 := grph.NewEdgeT("4", "5", 4.0)
-	edge56 := grph.NewEdgeT("5", "6", 2.0)
-
-	graph["0"] = []*grph.EdgeT[string]{edge01, edge02, edge03}
-	graph["1"] = []*grph.EdgeT[string]{edge01, edge12, edge16}
-	graph["2"] = []*grph.EdgeT[string]{edge02, edge12, edge24, edge25}
-	graph["3"] = []*grph.EdgeT[string]{edge03, edge34}
-	graph["4"] = []*grph.EdgeT[string]{edge24, edge34, edge45}
-	graph["5"] = []*grph.EdgeT[string]{edge25, edge45, edge56}
-	graph["6"] = []*grph.EdgeT[string]{edge16, edge56}
-
-	return graph
+func climbStairs(n int) int {
+	return 0
 }
