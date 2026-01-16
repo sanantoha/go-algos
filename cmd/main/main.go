7c8cb0e6f4fe3823ed310955c94afb84a1fc16d8
sanantoha <sanantoha@gmail.com>
2026-01-16 07:40:10 +0400

    +1 task completed.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -174,6 +175,7 @@ func main() {
 		"same_bst.go":                                        true,
 		"prim_min_spanning_tree_as_map.go":                   true,
 		"kruskal_min_spanning_tree.go":                       true,
+		"bellman_ford_as_map.go":                             true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -195,66 +197,24 @@ func main() {
 
 func runTask() {
 
-	graph := grph.NewEdgeWeightedGraph(6)
+	graph, err := grph.NewGraphAsAdjListFromFile("cmd/graph/bellman_ford/bellmanFord.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	graph.AddEdge(grph.NewEdge(0, 1, 7.0))
-	graph.AddEdge(grph.NewEdge(0, 2, 8.0))
-	graph.AddEdge(grph.NewEdge(1, 2, 3.0))
-	graph.AddEdge(grph.NewEdge(1, 3, 6.0))
-	graph.AddEdge(grph.NewEdge(2, 3, 4.0))
-	graph.AddEdge(grph.NewEdge(2, 4, 3.0))
-	graph.AddEdge(grph.NewEdge(3, 4, 2.0))
-	graph.AddEdge(grph.NewEdge(3, 5, 5.0))
-	graph.AddEdge(grph.NewEdge(4, 5, 2.0))
-	/*
-	   6 5
-	   0: 0-1 7.00
-	   1: 1-2 3.00  0-1 7.00
-	   2: 1-2 3.00  2-4 3.00
-	   3: 3-4 2.00
-	   4: 3-4 2.00  4-5 2.00  2-4 3.00
-	   5: 4-5 2.00
-	*/
-	fmt.Println(graph)
-	fmt.Println("=========================================")
-	fmt.Println(mst(graph))
-	fmt.Println("=========================================")
-	fmt.Println(mst1(graph))
-	fmt.Println("=========================================")
-
-	graph1 := grph.NewEdgeWeightedGraph(7)
-	graph1.AddEdge(grph.NewEdge(0, 1, 2.0))
-	graph1.AddEdge(grph.NewEdge(0, 2, 3.0))
-	graph1.AddEdge(grph.NewEdge(0, 3, 7.0))
-	graph1.AddEdge(grph.NewEdge(1, 2, 6.0))
-	graph1.AddEdge(grph.NewEdge(1, 6, 3.0))
-	graph1.AddEdge(grph.NewEdge(2, 4, 1.0))
-	graph1.AddEdge(grph.NewEdge(2, 5, 8.0))
-	graph1.AddEdge(grph.NewEdge(3, 4, 5.0))
-	graph1.AddEdge(grph.NewEdge(4, 5, 4.0))
-	graph1.AddEdge(grph.NewEdge(5, 6, 2.0))
-
-	/*
-	   7 6
-	   0: 0-1 2.00000  0-2 3.00000
-	   1: 0-1 2.00000  1-6 3.00000
-	   2: 2-4 1.00000  0-2 3.00000
-	   3: 3-4 5.00000
-	   4: 2-4 1.00000  3-4 5.00000
-	   5: 5-6 2.00000
-	   6: 5-6 2.00000  1-6 3.00000
-	*/
-	fmt.Println(graph1)
-	fmt.Println("=========================================")
-	fmt.Println(mst(graph1))
-	fmt.Println("=========================================")
-	fmt.Println(mst1(graph1))
+	fmt.Println(grph.PrintGraphAsAdjList(graph))
+	fmt.Println("=====================================")
+	// ShortestPath{Shortest=[-9.0, -20.0, -18.0, -2.0, -11.0], Prev=[4, 2, 4, 0, 1]}
+	sp := findShortestPath(graph, "0")
+	fmt.Println(sp)
+	cycle := findNegativeWeightCycle(graph, sp)
+	fmt.Println(cycle)
 }
 
-func mst(graph *grph.EdgeWeightedGraph) *grph.EdgeWeightedGraph {
+func findShortestPath(graph map[string][]*grph.EdgeT[string], start string) *grph.Pair[map[string]float64, map[string]string] {
 	return nil
 }
 
-func mst1(graph *grph.EdgeWeightedGraph) *grph.EdgeWeightedGraph {
+func findNegativeWeightCycle(graph map[string][]*grph.EdgeT[string], path *grph.Pair[map[string]float64, map[string]string]) []string {
 	return nil
 }
