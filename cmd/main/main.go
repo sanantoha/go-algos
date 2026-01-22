25d5ee564337a7b7e22bb15b85f0cbd7f096ee18
sanantoha <sanantoha@gmail.com>
2026-01-22 06:57:15 +0400

    +1 task completed.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/sanantoha/go-algos/internals/tree"
 )
 
 func main() {
@@ -176,6 +178,7 @@ func main() {
 		"longest_common_subsequence.go":                      true,
 		"course_schedule.go":                                 true,
 		"river_sizes.go":                                     true,
+		"binary_tree_tilt.go":                                true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -197,17 +200,111 @@ func main() {
 
 func runTask() {
 
-	matrix := [][]int{
-		{1, 0, 0, 1, 0},
-		{1, 0, 1, 0, 0},
-		{0, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1},
-		{1, 0, 1, 1, 0},
+	root := &tree.TreeNode{
+		Val: 1,
+		Left: &tree.TreeNode{
+			Val: 2,
+		},
+		Right: &tree.TreeNode{
+			Val: 3,
+		},
 	}
 
-	fmt.Println(riverSizes(matrix)) // [2, 1, 5, 2, 2]
+	fmt.Println(findTilt(root) == 1)
+
+	root1 := &tree.TreeNode{
+		Val: 4,
+		Left: &tree.TreeNode{
+			Val: 2,
+			Left: &tree.TreeNode{
+				Val: 3,
+			},
+			Right: &tree.TreeNode{
+				Val: 5,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 9,
+			Right: &tree.TreeNode{
+				Val: 7,
+			},
+		},
+	}
+
+	fmt.Println(findTilt(root1) == 15)
+
+	root2 := &tree.TreeNode{
+		Val: 21,
+		Left: &tree.TreeNode{
+			Val: 7,
+			Left: &tree.TreeNode{
+				Val: 1,
+				Left: &tree.TreeNode{
+					Val: 3,
+				},
+				Right: &tree.TreeNode{
+					Val: 3,
+				},
+			},
+			Right: &tree.TreeNode{
+				Val: 1,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 14,
+			Left: &tree.TreeNode{
+				Val: 2,
+			},
+			Right: &tree.TreeNode{
+				Val: 2,
+			},
+		},
+	}
+
+	fmt.Println(findTilt(root2) == 9)
 }
 
-func riverSizes(matrix [][]int) []int {
-	return nil
+/**
+ * https://leetcode.com/problems/binary-tree-tilt/
+ *
+ * Given the root of a binary tree, return the sum of every tree node's tilt.
+ *
+ * The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right
+ * subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0.
+ * The rule is similar if the node does not have a right child.
+ *
+ *         1
+ *       /  \
+ *     2     3
+ *
+ * Input: root = [1,2,3]
+ * Output: 1
+ * Explanation:
+ * Tilt of node 2 : |0-0| = 0 (no children)
+ * Tilt of node 3 : |0-0| = 0 (no children)
+ * Tilt of node 1 : |2-3| = 1 (left subtree is just left child, so sum is 2; right subtree is just right child, so sum is 3)
+ * Sum of every tilt : 0 + 0 + 1 = 1
+ *
+ *
+ *                  4
+ *               /    \
+ *             2       9
+ *           /  \       \
+ *         3     5       7
+ *
+ *
+ * Input: root = [4,2,9,3,5,null,7]
+ * Output: 15
+ * Explanation:
+ * Tilt of node 3 : |0-0| = 0 (no children)
+ * Tilt of node 5 : |0-0| = 0 (no children)
+ * Tilt of node 7 : |0-0| = 0 (no children)
+ * Tilt of node 2 : |3-5| = 2 (left subtree is just left child, so sum is 3; right subtree is just right child, so sum is 5)
+ * Tilt of node 9 : |0-7| = 7 (no left child, so sum is 0; right subtree is just right child, so sum is 7)
+ * Tilt of node 4 : |(3+5+2)-(9+7)| = |10-16| = 6 (left subtree values are 3, 5, and 2, which sums to 10; right subtree values are 9 and 7, which sums to 16)
+ * Sum of every tilt : 0 + 0 + 0 + 2 + 7 + 6 = 15
+ */
+
+func findTilt(root *tree.TreeNode) int {
+	return -1
 }
