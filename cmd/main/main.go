77f1b8211eb80c59fc5a6effb38c2154d2477ac4
sanantoha <sanantoha@gmail.com>
2026-02-19 06:57:14 +0400

    +1 task completed.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
 
 	"github.com/sanantoha/go-algos/internals/tree"
@@ -40,6 +41,7 @@ func main() {
 		"subarray_sort.go":                                   true,
 		"rotting_oranges.go":                                 true,
 		"construct_binary_tree_from_preorder_and_inorder.go": true,
+		"all_elements_in_two_binary_search_trees.go":         true,
 	}
 
 	rand.Shuffle(len(tasks), func(i, j int) {
@@ -61,13 +63,52 @@ func main() {
 
 func runTask() {
 
-	preorder := []int{3, 9, 20, 15, 7}
-	inorder := []int{9, 3, 15, 20, 7}
-	// TreeNode{Val=3, Left=TreeNode{Val=9, Left=nil, Right=nil}, Right=TreeNode{Val=20, Left=TreeNode{Val=15, Left=nil, Right=nil}, Right=TreeNode{Val=7, Left=nil, Right=nil}}}
-	root := buildTree(preorder, inorder)
-	fmt.Println(root)
+	root1 := &tree.TreeNode{
+		Val: 9,
+		Left: &tree.TreeNode{
+			Val: 2,
+		},
+		Right: &tree.TreeNode{
+			Val: 12,
+			Left: &tree.TreeNode{
+				Val: 11,
+			},
+			Right: &tree.TreeNode{
+				Val: 15,
+			},
+		},
+	}
+
+	root2 := &tree.TreeNode{
+		Val: 10,
+		Left: &tree.TreeNode{
+			Val: 5,
+			Left: &tree.TreeNode{
+				Val: 3,
+				Left: &tree.TreeNode{
+					Val: 1,
+				},
+				Right: &tree.TreeNode{
+					Val: 4,
+				},
+			},
+			Right: &tree.TreeNode{
+				Val: 6,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 16,
+		},
+	}
+
+	expected := []int{1, 2, 3, 4, 5, 6, 9, 10, 11, 12, 15, 16}
+
+	res := getAllElements(root1, root2)
+
+	fmt.Println(res)
+	fmt.Println(reflect.DeepEqual(expected, res))
 }
 
-func buildTree(preorder []int, inorder []int) *tree.TreeNode {
+func getAllElements(root1, root2 *tree.TreeNode) []int {
 	return nil
 }
